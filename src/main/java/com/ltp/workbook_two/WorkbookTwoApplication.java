0c31ddfbb4b5f1e8212447bc093ff1c513de89f1
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-01 16:41:42 +0900

    first commit

---
+++ src/main/java/com/ltp/workbook_two/WorkbookTwoApplication.java
@@ -0,0 +1,14 @@
+package com.ltp.workbook_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbookTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbookTwoApplication.class, args);
+	}
+	
+
+}
